c4ad9a85311d578acff60384d9932ee6007280ee
luke.lane <lukelane1@gmail.com>
2024-07-15 08:54:04 +0900

    First prototype of Timeline control

---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using WpfTimelineControl;
+using System;
+
+namespace TestApp
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var timelineData = new TimelineData();
+
+            timelineData.AddEntries(
+                new TimelineEntry(new DateTime(2024, 1, 10, 8, 6, 14), TimeSpan.FromSeconds(28), "Event 1"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 8, 6, 20), TimeSpan.FromSeconds(344), "Event 2"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 8, 27, 1), TimeSpan.FromSeconds(31), "Event 3"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 9, 23, 33), TimeSpan.FromSeconds(1001), "Event 4"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 9, 59, 22), TimeSpan.FromSeconds(550), "Event 5"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 10, 16, 19), TimeSpan.FromSeconds(97), "Event 6"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 10, 30, 47), TimeSpan.FromSeconds(262), "Event 7"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 11, 44, 53), TimeSpan.FromSeconds(61), "Event 8"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 12, 00, 14), TimeSpan.FromSeconds(50), "Event 9"),
+                new TimelineEntry(new DateTime(2024, 1, 10, 12, 17, 9), TimeSpan.FromSeconds(212), "Event 10"));
+
+            myTimeline.TimelineData = timelineData;
+        }
+    }
+}
---
+++ WpfTimelineControl/Timeline.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace WpfTimelineControl
+{
+    public partial class Timeline
+    {
+        public Timeline()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public TimelineData TimelineData
+        {
+            get { return (TimelineData)GetValue(TimelineDataProperty); }
+            set { SetValue(TimelineDataProperty, value); }
+        }
+
+        public static readonly DependencyProperty TimelineDataProperty =
+            DependencyProperty.Register("TimelineData", typeof(TimelineData), typeof(Timeline), new PropertyMetadata(new TimelineData()));
+    }
+}
